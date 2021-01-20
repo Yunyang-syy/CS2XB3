bb80b69750d9fc1d06df2822afec559d7231058d
niey12 <59624361+niey12@users.noreply.github.com>
2021-01-20 21:48:27 +0800

    Yuhong code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
+def are_vaild_groups()def are_valid_groups(litsOfStudent, listOfGroup):
+    for i in litsOfStudent:
+        for j in listOfGroup:
+            if i in j:
+                break
+        print ("True")
 
+
+print (are_valid_groups(["1","2","3"], [["1","2","3"],["3"]])):
